01b1e8e59a75beb9d22b7b894c0045873e3496b9
Arthur Philadelpho <arthur22rj@gmail.com>
2016-11-22 18:04:30 -0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+(function(){
+	function configure($stateProvider, $locationProvider){
+			$locationProvider
+					.html5Mode({
+							enabled: true,
+							requireBase: false
+					});
+
+			$stateProvider
+					.state('home', {
+							url:'/',
+							controller: 'homeCtrl as home',
+							templateUrl: '/templates/home.html'
+					});
+	}
+
+	angular
+			.module('blocChat', ['ui.router', 'firebase'])
+			.config(configure);
+
+})();
