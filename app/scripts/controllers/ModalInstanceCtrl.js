6b08ae3a7cb479eace8cafe3286f404862070033
Arthur Philadelpho <arthur22rj@gmail.com>
2016-11-26 01:26:18 -0200

    Still having trouble creating the modal

---
+++ app/scripts/controllers/ModalInstanceCtrl.js
@@ -0,0 +1,23 @@
+(function(){
+	function ModalInstanceCtrl($uibModalInstance, items){
+		var $ctrl = this;
+    $ctrl.items = items;
+    $ctrl.selected = {
+      item: $ctrl.items[0]
+    };
+
+    $ctrl.ok = function () {
+      $uibModalInstance.close($ctrl.selected.item);
+    };
+
+    $ctrl.cancel = function () {
+      $uibModalInstance.dismiss('cancel');
+    };
+
+	}
+
+	angular
+			.module('blocChat')
+			.controller('ModalInstanceCtrl', ['$uibModalInstance', 'items', ModalInstanceCtrl ]);
+
+})();
