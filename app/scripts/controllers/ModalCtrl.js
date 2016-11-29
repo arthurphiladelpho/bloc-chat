18b65d7bbd3f31d0283064881de15e9d156e78e5
Arthur Philadelpho <arthur22rj@gmail.com>
2016-11-28 23:45:12 -0200

    Having trouble with ()

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -3,6 +3,16 @@
 		$scope.close = function(){
       $modalInstance.dismiss('Close window.');
     };
+    // Write function to retrieve value of placeholder and user Angular Fire's $add 
+    // to create a new database record and add it to the sidebar.
+    $scope.add = function(){
+    	// retrieve value of placeholder and console.log it
+    	var newName = document.getElementById('newRoom').value;
+    	console.log('adding ' + newName + ' room...');
+    	// When I try to use $add on the rooms array I get a Reference Error, rooms is not defined.
+    	// How can I reference it?
+    	rooms.$add(newName);
+    };
 
 	}
 
